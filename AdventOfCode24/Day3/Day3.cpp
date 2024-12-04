9795515d5cbb3b5b4b68f651c00cb57f7253951d
Steve Aarnio <saarnio@hotmail.com>
2024-12-03 17:44:35 -0800

    Advent of Code 2024 - Day 3
    Commit simple solution for part 2. Regex is the way.

--- AdventOfCode24/Day3/Day3.cpp
+++ AdventOfCode24/Day3/Day3.cpp
@@ -7,6 +7,38 @@
 #include <string>
 #include <vector>
 
+static std::string::const_iterator get_next_disable(
+    const std::string::const_iterator& line_start,
+    const std::string::const_iterator& line_end )
+{
+    static const std::regex off( "don't\\(\\)" );
+
+    std::string::const_iterator disable_start( line_end );
+    std::smatch reg_match;
+    if( std::regex_search( line_start, line_end, reg_match, off ) )
+    {
+        disable_start = reg_match.suffix().first;
+    }
+
+    return disable_start;
+}
+
+static std::string::const_iterator get_next_enable(
+    const std::string::const_iterator& line_start,
+    const std::string::const_iterator& line_end )
+{
+    static const std::regex on( "do\\(\\)" );
+
+    std::string::const_iterator enable_start( line_end );
+    std::smatch reg_match;
+    if( std::regex_search( line_start, line_end, reg_match, on ) )
+    {
+        enable_start = reg_match.suffix().first;
+    }
+
+    return enable_start;
+}
+
 static bool read_input( std::vector<std::pair<int32_t, int32_t>>& mul_values )
 {
     // Open the input file and read the data.
@@ -19,17 +51,52 @@ static bool read_input( std::vector<std::pair<int32_t, int32_t>>& mul_values )
     // Integers in the file represent levels.
     // Each line contains multiple levels separated by a space.
     static const std::regex mul_regex( "mul\\(([0-9]+),([0-9]+)\\)" );
+
+    bool is_enabled = true;
     std::string line;
     while( std::getline( file, line ) )
     {
         std::string::const_iterator line_start( line.cbegin() );
-        std::smatch mul_match;
-        while( std::regex_search( line_start, line.cend(), mul_match, mul_regex ) )
+        std::string::const_iterator disable_start( line.cend() );
+
+        if( !is_enabled )
         {
+            line_start = get_next_enable( line.cbegin(), line.cend() );
+            is_enabled = true;
+        }
+        disable_start = get_next_disable( line_start, line.cend() );
+
+        std::smatch reg_match;
+        while( std::regex_search( line_start, line.cend(), reg_match, mul_regex ) )
+        {
+            if( reg_match.suffix().first >= disable_start &&
+                line.cend() != disable_start )
+            {
+                // mul() operator found after a don't() operation.
+                // Look for the next do() operation from which to restart the
+                // mul() operator search.
+                line_start = get_next_enable( reg_match.suffix().first, line.cend() );
+
+                // If there is no next do() operator, then disable the mul() accumator
+                // for the beginning of the next line.
+                if( line.cend() == line_start )
+                {
+                    is_enabled = false;
+                    break;
+                }
+
+                // Also find the next don't() operation.
+                disable_start = get_next_disable( line_start, line.cend() );
+
+                // Restart search for mul() operators at new line_start (after next do())
+                continue;
+            }
+
             mul_values.emplace_back(
-                std::make_pair( std::atoi( mul_match[ 1 ].str().c_str() ), std::atoi( mul_match[ 2 ].str().c_str() ) )
+                std::make_pair( std::atoi( reg_match[ 1 ].str().c_str() ), std::atoi( reg_match[ 2 ].str().c_str() ) )
             );
-            line_start = mul_match.suffix().first;
+
+            line_start = reg_match.suffix().first;
         }
     }
 
