754f8f3fc2c6bc487b4e9100aaa40941c0945077
Steve Aarnio <saarnio@hotmail.com>
2024-12-08 22:08:56 -0800

    Advent of Code 2024 - Day 7
    Complete part 2 solution with generalization and a simple optimization
    to skip checking the data with n + 1 operators if that data has already
    been verified good with n operators.

--- AdventOfCode24/Day7/Day7.cpp
+++ AdventOfCode24/Day7/Day7.cpp
@@ -14,7 +14,7 @@ static bool read_input(
 {
     // Open the input file and read the data.
     // Step 1: open the input file.
-    std::ifstream file( ".\\Calibrations_full.txt" );
+    std::ifstream file( ".\\Calibrations.txt" );
 
     // Step 2: read each line to get the calibration result and input values.
     static const std::regex numbers( "([0-9]+)" );
@@ -53,60 +53,116 @@ static bool read_input(
 }
 
 static size_t check_calibration(
-    std::map<size_t, std::vector<size_t>>& calibration_data
+    std::map<size_t, std::vector<size_t>>& calibration_data,
+    const size_t num_ops
 )
 {
     // Two operators are available + and *.
     // Operations always proceed left to right, regardless of real-world
     // operator precedence.
     size_t total_result = 0;
-    for( auto& calibration : calibration_data )
+    std::map<size_t, std::vector<size_t>>::iterator cal_it = calibration_data.begin();
+    while( calibration_data.end() != cal_it )
     {
         // Brute force it.
         // Try all of the combinations until the correct solution is found or
         // none of the combinations results in the correct answer.
 
-        // There are two operators, can use 0 and 1 to represent the possible
-        // operator combinations. For example, treat + = 0 and * = 1.
-        // If there are three operator slots there are eight possible
-        // operator combinations (2^3):
-        // a)  + + + = b000 = 0;  e) * + + = b100 = 4
-        // b)  + + * = b001 = 1;  f) * + * = b101 = 5
-        // c)  + * + = b010 = 2;  g) * * + = b110 = 6
-        // d)  + * * = b011 = 3;  h) * * * = b111 = 7
+        // There are some number of operators, use 0, 1, 2 ... num_ops - 1
+        // to represent the operator operations. For example, if there are
+        // three operators, treat '+' = 0, '*' = 1, and || = 2.
+        // In this case, if there are two operator slots there are 9 possible
+        // operator combinations (3^2):
+        // a)  + + = t00 = 0;  c)  * + = t10 = 3;  f)  | + = t20 = 6;
+        // b)  + * = t01 = 1;  d)  * * = t11 = 4;  g)  | * = t21 = 7;
+        // c)  + | = t02 = 2;  e)  * | = t12 = 5;  h)  | | = t22 = 8;
 
         // So, loop over the number of possible operators and convert each
         // loop index into the corresponding operations based on the above
         // representation.
-        size_t operator_count = static_cast<size_t>( std::pow( 2, calibration.second.size() - 1 ) );
-        --operator_count; // Zero-based counting
-        for( size_t op_idx = 0; op_idx <= operator_count; ++op_idx )
+        const std::pair<size_t, std::vector<size_t>>& calibration = *cal_it;
+        bool is_good = false;
+
+        // First, how many operations are there in this calibration calc?
+        size_t cal_op_count = static_cast<size_t>( std::pow( num_ops, calibration.second.size() - 1 ) );
+        --cal_op_count; // Zero-based counting on the combinations
+
+        // Loop over each operation combination and convert the index into
+        // appropriate operations between each of the values, left to right.
+        for( size_t op_combo = 0; op_combo <= cal_op_count; ++op_combo )
         {
-            size_t op = op_idx;
-            std::vector<size_t>::iterator val = calibration.second.begin();
-            size_t value = *val;
-            ++val;
+            std::vector<size_t>::const_iterator val = calibration.second.begin();
+            size_t value = *val; ++val;
+
+            // Need to break the operation combination into it's constituent
+            // parts. Start with the largest power of N and work down.
+            size_t ops = op_combo;
+
+            // Find the largest power of N that is needed for this combination.
+            // It's the number of operation slots minus 1 or the number of
+            // values minus 2.
+            size_t op_idx_power = calibration.second.size() - 2;
+
+            // Loop over all of the values
             while( calibration.second.end() != val )
             {
-                if( op & 1 )
+                // Base N value for this operation index.
+                size_t power_of = static_cast<size_t>( std::pow( num_ops, op_idx_power ) );
+
+                // Translate that number to the corresponding operation.
+                // How many "Base Ns" does this value have?
+                size_t which_op = ops / power_of;
+                switch( which_op )
                 {
-                    value *= *val;
-                }
-                else
-                {
+                case 0:
                     value += *val;
+                    break;
+                case 1:
+                    value *= *val;
+                    break;
+                case 2:
+                    // Need to shift value to the left by ten for each power of
+                    // ten in the new value (10^0 is a power of ten...).
+                    {
+                        size_t new_val = *val;
+                        while( new_val )
+                        {
+                            value *= 10;
+                            new_val /= 10;
+                        }
+                    }
+                    value += *val;
+                    break;
                 }
 
+                // Move to the next value in the list
                 ++val;
-                op >>= 1;
+
+                // Remove this operation from the operation list and move to
+                // the next operation.
+                ops -= ( power_of * which_op );
+                --op_idx_power;
             }
 
             if( calibration.first == value )
             {
-                // Found a valid operational sequence.
-                total_result += calibration.first;
+                // Found a valid operational sequence, stop the search
+                is_good = true;
                 break;
             }
+        }
+
+        if( is_good )
+        {
+            // Found a valid operational sequence, add the result to the total.
+            total_result += calibration.first;
+
+            // Remove this data from the map to avoid checking it again.
+            cal_it = calibration_data.erase( cal_it, std::next( cal_it ) );
+        }
+        else
+        {
+            ++cal_it;
         }
     }
 
@@ -121,6 +177,9 @@ int main()
         return -1;
     }
 
-    size_t calibration_result = check_calibration( calibration_data );
-    std::cout << "Calibration total = " << calibration_result << "\n";
+    size_t calibration_result = check_calibration( calibration_data, 2 );
+    std::cout << "Calibration total (with 2 operators) = " << calibration_result << "\n";
+
+    calibration_result += check_calibration( calibration_data, 3 );
+    std::cout << "Calibration total (with 3 operators) = " << calibration_result << std::endl;
 }
