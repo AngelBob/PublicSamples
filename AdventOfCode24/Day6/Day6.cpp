182af8bfa96ed80ceabced0ae436b59f42d71385
Steve Aarnio <saarnio@hotmail.com>
2024-12-08 19:08:16 -0800

    Advent of Code 2024 - Day 6
    Commit part 2 solution. It's wrong, but the debug is eluding me.

--- AdventOfCode24/Day6/Day6.cpp
+++ AdventOfCode24/Day6/Day6.cpp
@@ -65,14 +65,28 @@ static bool read_input(
     return ( height == grid[ 0 ].size() ); // Expecting a square grid
 }
 
-static size_t do_guard_walk(
+static std::pair<size_t, size_t> do_guard_walk(
     std::vector<std::string>& grid,
     std::tuple<size_t, size_t, enum WALK_DIR>& guard_status
 )
 {
     const size_t max_idx = grid.size();
 
+    // Keep track of the directions in which the guard crosses each location.
+    std::vector<std::vector<std::array<bool, WALK_DIR::WALK_END>>> visited_dir;
+    for( size_t row = 0; row < grid.size(); ++row )
+    {
+        std::vector<std::array<bool, WALK_DIR::WALK_END>> col_dirs;
+        for( size_t col = 0; col < grid.size(); ++col )
+        {
+            col_dirs.emplace_back( std::array<bool, WALK_DIR::WALK_END>{{ false, false, false, false }} );
+        }
+
+        visited_dir.emplace_back( col_dirs );
+    }
+
     size_t location_count = 1; // Mark the starting location as visited.
+    size_t route_loop_count = 0; // Count the number of route loop options.
     while( 1 )
     {
         const walk_data_t this_walk = walk_data[ std::get<2>( guard_status ) ];
@@ -81,6 +95,9 @@ static size_t do_guard_walk(
 
         // Mark the guard's current location as visited.
         grid[ row ][ col ] = this_walk.dir_indicator;
+
+        // Mark the direction in which the guard is traveling.
+        visited_dir[ row ][ col ][ this_walk.dir ] = true;
 
         // Try to move the guard
         col += this_walk.x_inc;
@@ -112,9 +129,50 @@ static size_t do_guard_walk(
             std::get<0>( guard_status ) = col;
             std::get<1>( guard_status ) = row;
         }
+
+        // Check if the guard can be placed in a route loop. Route loops occur
+        // if the guard's direction can be changed and the new route includes
+        // a location that the guard has already visited while traveling in the
+        // same direction.
+        std::vector<std::vector<std::array<bool, WALK_DIR::WALK_END>>> visited_loop_dir = visited_dir;
+
+        size_t dir_t = ( static_cast<size_t>( std::get<2>( guard_status ) ) + 1 ) % WALK_DIR::WALK_END;
+        size_t col_t = col - this_walk.x_inc;
+        size_t row_t = row - this_walk.y_inc;
+        while( 1 )
+        {
+            visited_loop_dir[ row_t ][ col_t ][ dir_t ] = true;
+
+            col_t += walk_data[ dir_t ].x_inc;
+            row_t += walk_data[ dir_t ].y_inc;
+
+            if( col_t < 0 || max_idx <= col_t ||
+                row_t < 0 || max_idx <= row_t )
+            {
+                // Guard is going to move off the map, no route loop possible.
+                break;
+            }
+            else if( '#' == grid[ row_t ][ col_t ] )
+            {
+                // Guard is about to hit an obstacle, rotate path 90 degrees and
+                // try stepping in the new direction.
+                dir_t = ( dir_t + 1 ) % WALK_DIR::WALK_END;
+                continue;
+            }
+            else if( visited_loop_dir[ row_t ][ col_t ][ dir_t ] )
+            {
+                // Route loop created!
+#ifdef _DEBUG
+                // Print the location
+                std::cout << row << "," << col << std::endl;
+#endif
+                ++route_loop_count;
+                break;
+            }
+        }
     }
 
-    return location_count;
+    return std::make_pair( location_count, route_loop_count );
 }
 
 int main()
@@ -126,6 +184,7 @@ int main()
         return -1;
     }
 
-    size_t count = do_guard_walk( grid, guard_status );
-    std::cout << "Guard touches " << count << " distinct locations\n";
+    std::pair<size_t, size_t> counts = do_guard_walk( grid, guard_status );
+    std::cout << "Guard touches " << counts.first << " distinct locations and\n";
+    std::cout << "there are " << counts.second << " route loop options." << std::endl;
 }
