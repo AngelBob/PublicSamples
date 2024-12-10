f9a423b064ff567dadb4d7789a62cde5fb7fe0dd
Steve Aarnio <saarnio@hotmail.com>
2024-12-10 00:40:53 -0800

    Advent of Code 2024 - Day 10
    Commit part 1 solution. Recursion is the way.

--- AdventOfCode24/Day10/Day10.cpp
+++ AdventOfCode24/Day10/Day10.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 static bool read_input(
-    std::vector<std::string>& topo_map
+    std::vector<std::vector<int32_t>>& topo_map
 )
 {
     // Open the input file and read the data.
@@ -17,18 +17,102 @@ static bool read_input(
     std::string line;
     while( std::getline( file, line ) )
     {
-        topo_map.emplace_back( line );
+        std::vector<int32_t> topo;
+        topo.reserve( line.length() );
+
+        for( size_t i = 0; i < line.length(); ++i )
+        {
+            const char digit = line[ i ];
+            topo.emplace_back( digit - '0' );
+        }
+        topo_map.emplace_back( topo );
     }
 
     // Step 3: return success or failure.
     return true;
 }
 
+static int32_t find_path_step(
+    std::vector<std::vector<int32_t>>& topo_map,
+    const size_t start_row,
+    const size_t start_col
+)
+{
+    // Is the trail at the top?
+    if( 9 == topo_map[ start_row ][ start_col ] )
+    {
+        // Don't count this one again...
+        topo_map[ start_row ][ start_col ] = -1;
+
+        // Add this to the reachable peaks count
+        return 1;
+    }
+
+    // Try stepping north
+    int32_t sum_scores = 0;
+    if( 0 < start_row )
+    {
+        if( topo_map[ start_row - 1 ][ start_col ] ==
+            ( topo_map[ start_row ][ start_col ] + 1 ) )
+        {
+            sum_scores += find_path_step( topo_map, start_row - 1, start_col );
+        }
+    }
+
+    // Try stepping south
+    if( start_row < topo_map.size() - 1 )
+    {
+        if( topo_map[ start_row + 1 ][ start_col ] ==
+            ( topo_map[ start_row ][ start_col ] + 1 ) )
+        {
+            sum_scores += find_path_step( topo_map, start_row + 1, start_col );
+        }
+    }
+
+    // Try stepping east
+    if( start_col < topo_map[ 0 ].size() - 1 )
+    {
+        if( topo_map[ start_row ][ start_col + 1 ] ==
+            ( topo_map[ start_row ][ start_col ] + 1 ) )
+        {
+            sum_scores += find_path_step( topo_map, start_row, start_col + 1 );
+        }
+    }
+
+    // Try stepping west
+    if( 0 < start_col )
+    {
+        if( topo_map[ start_row ][ start_col - 1 ] ==
+            ( topo_map[ start_row ][ start_col ] + 1 ) )
+        {
+            sum_scores += find_path_step( topo_map, start_row, start_col - 1 );
+        }
+    }
+
+    return sum_scores;
+}
+
 int main()
 {
-    std::vector<std::string> topo_map;
+    std::vector<std::vector<int32_t>> topo_map;
     if( !read_input( topo_map ) )
     {
         return -1;
     }
+
+    int32_t sum_scores = 0;
+    for( size_t row = 0; row < topo_map.size(); ++row )
+    {
+        for( size_t col = 0; col < topo_map[ 0 ].size(); ++col )
+        {
+            if( 0 == topo_map[ row ][ col ] )
+            {
+                // Send a copy of the map - find path step will modify.
+                std::vector<std::vector<int32_t>> temp_map = topo_map;
+                sum_scores += find_path_step( temp_map, row, col );
+            }
+        }
+    }
+
+    std::cout << "The sum of the path scores is: " << sum_scores << "\n";
 }
