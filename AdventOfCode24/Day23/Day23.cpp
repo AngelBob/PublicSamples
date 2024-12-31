2f914ae90fc25c1fec6e33da514dd782f73bad2f
Steve Aarnio <saarnio@hotmail.com>
2024-12-31 12:00:22 -0800

    Advent of Code 2024 - Day 23
    Commit part one solution.

--- AdventOfCode24/Day23/Day23.cpp
+++ AdventOfCode24/Day23/Day23.cpp
@@ -1,12 +1,15 @@
 // Day23.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <set>
 #include <string>
+#include <vector>
 
 static bool read_input(
-    std::multimap<std::string, std::string>& lan_pairs
+    std::map<std::string, std::set<std::string>>& lan_map
 )
 {
     // Open the input file and read the data.
@@ -20,19 +23,56 @@ static bool read_input(
         std::string first( { line[ 0 ], line[ 1 ] } );
         std::string second( { line[ 3 ], line[ 4 ] } );
 
-        lan_pairs.insert( std::make_pair( first, second ) );
-        lan_pairs.insert( std::make_pair( second, first ) );
+        lan_map[ first ].insert( second );
+        lan_map[ second ].insert( first );
     }
 
     // Step 3: return success or failure.
-    return true;
+    return ( 0 != lan_map.size() );
+}
+
+static void map_t_lans(
+    const std::map<std::string, std::set<std::string>>& lan_maps,
+    std::set<std::vector<std::string>>& lans_with_t
+)
+{
+    for( const std::pair<std::string, std::set<std::string>>& t_base : lan_maps )
+    {
+        // All computers are in the map, skip those that don't start with "t".
+        if( 't' != t_base.first[ 0 ] )
+        {
+            continue;
+        }
+
+        // For all t? computers, search their links for a matching t?.
+        for( const std::string& link : t_base.second )
+        {
+            for( const std::string& loop : lan_maps.at( link ) )
+            {
+                if( t_base.first != loop && lan_maps.at( loop ).contains( t_base.first ) )
+                {
+                    std::vector<std::string> temp{ t_base.first, link, loop };
+                    std::ranges::sort( temp );
+                    if( lans_with_t.contains( temp ) )
+                    {
+                        continue;
+                    }
+                    lans_with_t.insert( temp );
+                }
+            }
+        }
+    }
 }
 
 int main()
 {
-    std::multimap<std::string, std::string> lan_pairs;
-    if( !read_input( lan_pairs ) )
+    std::map<std::string, std::set<std::string>> lan_maps;
+    if( !read_input( lan_maps ) )
     {
         return -1;
     }
+
+    std::set<std::vector<std::string>> lans_with_t;
+    map_t_lans( lan_maps, lans_with_t );
+    std::cout << "There are " << std::to_string( lans_with_t.size() ) << " LANs with a t? computer.\n";
 }
