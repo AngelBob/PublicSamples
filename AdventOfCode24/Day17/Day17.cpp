70762c1acc534c6967e091f1f3130220265f731f
Steve Aarnio <saarnio@hotmail.com>
2025-01-15 16:36:16 -0800

    Advent of Code 2024 - Day 17
    Refactor part 1 solution.

--- AdventOfCode24/Day17/Day17.cpp
+++ AdventOfCode24/Day17/Day17.cpp
@@ -8,15 +8,189 @@
 #include <string>
 #include <vector>
 
-enum REGISTERS
+class computer
 {
-    REG_A = 0,
-    REG_B,
-    REG_C
+public:
+    computer() = delete;
+
+    computer( const std::array<uint64_t, 3> r ) :
+        registers( r )
+    {
+    }
+
+    void run(
+        const std::vector<uint8_t>& op_codes
+    )
+    {
+        while( ip < op_codes.size() )
+        {
+            uint8_t a = op_codes[ ip ];
+            uint8_t b = op_codes[ ip + 1 ];
+            exec( a, b );
+        }
+    }
+
+    const std::vector<uint64_t>& get_output( void ) const
+    {
+        return output;
+    }
+
+private:
+    enum REGISTERS
+    {
+        REG_A = 0,
+        REG_B,
+        REG_C
+    };
+
+    void exec(
+        const uint8_t a,
+        const uint8_t b
+    )
+    {
+        bool inc_ip = true;
+
+        switch( a )
+        {
+        case 0:
+            // adv - division out to A
+            adv( b );
+            break;
+        case 1:
+            // bxl - bitwise OR w/ operand
+            bxl( b );
+            break;
+        case 2:
+            // bst - mod 8
+            bst( b );
+            break;
+        case 3:
+            // jnz - jump if not zero
+            if( registers[ REG_A ] )
+            {
+                ip = b;
+                inc_ip = false;
+            }
+            break;
+        case 4:
+            // bxc - bitwise OR on B & C
+            bxc( b );
+            break;
+        case 5:
+            // out - output
+            out( b );
+            break;
+        case 6:
+            // bdv - division out to B
+            bdv( b );
+            break;
+        case 7:
+            // cdv - division out to C
+            cdv( b );
+            break;
+        }
+
+        ip += inc_ip ? 2 : 0;
+    }
+
+    uint64_t get_combo(
+        const uint8_t combo
+    )
+    {
+        uint64_t result = 0;
+        if( combo < 4 )
+        {
+            result = combo;
+        }
+        else if( combo < 7 )
+        {
+            result = registers[ combo - 4 ];
+        }
+        else
+        {
+            throw std::runtime_error( "Invalid value for combo" );
+        }
+
+        return result;
+    }
+
+    uint64_t div(
+        const uint8_t combo
+    )
+    {
+        double numerator = static_cast<double>( registers[ REG_A ] );
+        double denominator = static_cast<double>( get_combo( combo ) );
+
+        denominator = std::pow( 2, denominator );
+
+        return static_cast<uint64_t>( numerator / denominator );
+    }
+
+    void adv(
+        const uint8_t combo
+    )
+    {
+        registers[ REG_A ] = div( combo );
+    }
+
+    void bdv(
+        const uint8_t combo
+    )
+    {
+        registers[ REG_B ] = div( combo );
+    }
+
+    void cdv(
+        const uint8_t combo
+    )
+    {
+        registers[ REG_C ] = div( combo );
+    }
+
+    void bxl(
+        const uint8_t combo
+    )
+    {
+        uint64_t a = registers[ REG_B ];
+        uint64_t b = combo;
+
+        registers[ REG_B ] = a ^ b;
+    }
+
+    void bxc(
+        const uint8_t combo
+    )
+    {
+        uint64_t a = registers[ REG_B ];
+        uint64_t b = registers[ REG_C ];
+
+        registers[ REG_B ] = a ^ b;
+    }
+
+    void bst(
+        const uint8_t combo
+    )
+    {
+        uint64_t a = get_combo( combo );
+
+        registers[ REG_B] = ( a & 0x7 );
+    }
+
+    void out(
+        const uint8_t combo
+    )
+    {
+        uint64_t a = get_combo( combo );
+        output.emplace_back( a & 0x7 );
+    }
+
+    size_t ip{ 0 };
+    std::array<uint64_t, 3> registers;
+    std::vector<uint64_t> output;
 };
 
 static bool read_input(
-    std::array<uint32_t, 3>& registers,
+    std::array<uint64_t, 3>& registers,
     std::vector<uint8_t>& op_codes
 )
 {
@@ -27,15 +201,15 @@ static bool read_input(
     // Step 2: read each line and insert individual characters into the grid.
     std::string line;
     size_t reg_idx = 0;
-    while( std::getline( file, line, ',' ) )
+    while( std::getline( file, line, ',') )
     {
-        registers[ reg_idx ] = stol( line );
+        registers[ reg_idx ] = stoull( line );
         reg_idx++;
     }
 
     file.close();
     file.open( ".\\Asm.txt" );
-    while( std::getline( file, line, ',' ) )
+    while( std::getline( file, line, ',') )
     {
         op_codes.emplace_back( static_cast<uint8_t>( stoul( line ) ) );
     }
@@ -44,188 +218,21 @@ static bool read_input(
     return true;
 }
 
-static uint32_t get_combo(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    uint32_t result = 0;
-    switch( combo )
-    {
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-        result = combo;
-        break;
-    case 4:
-    case 5:
-    case 6:
-        result = registers[ ( combo - 4 ) ];
-        break;
-    case 7:
-    default:
-        __debugbreak();  // Should not happen
-    }
-
-    return result;
-}
-
-static double do_div(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    double numerator = registers[ REG_A ];
-    double denominator = get_combo( combo, registers );
-
-    denominator = std::pow( 2, denominator );
-
-    return numerator / denominator;
-}
-
-static void do_adv(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    registers[ REG_A ] = static_cast<uint32_t>( do_div( combo, registers ) );
-}
-
-static void do_bdv(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    registers[ REG_B ] = static_cast<uint32_t>( do_div( combo, registers ) );
-}
-
-static void do_cdv(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    registers[ REG_C ] = static_cast<uint32_t>( do_div( combo, registers ) );
-}
-
-static void do_bxl(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    uint32_t a = registers[ REG_B ];
-    uint32_t b = combo;
-
-    registers[ REG_B ] = a ^ b;
-}
-
-static void do_bxc(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    uint32_t a = registers[ REG_B ];
-    uint32_t b = registers[ REG_C ];
-
-    registers[ REG_B ] = a ^ b;
-}
-
-static void do_bst(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers
-)
-{
-    uint32_t a = get_combo( combo, registers );
-
-    registers[ REG_B ] = ( a & 0x7 );
-}
-
-static void do_out(
-    const uint8_t combo,
-    std::array<uint32_t, 3>& registers,
-    std::string& output
-)
-{
-    uint32_t a = get_combo( combo, registers );
-
-    if( output.length() )
-    {
-        output += ",";
-    }
-    output += std::to_string( a & 0x7 );
-}
-
-static void execute(
-    std::array<uint32_t, 3>& registers,
-    const std::vector<uint8_t>& op_codes,
-    std::string& output
-)
-{
-    std::vector<uint8_t>::const_iterator op = op_codes.cbegin();
-    while( op_codes.cend() != op )
-    {
-        std::vector<uint8_t>::const_iterator combo = std::next( op );
-        if( op_codes.cend() == combo )
-        {
-            // This shouldn't happen...
-            __debugbreak();
-        }
-
-        switch( *op )
-        {
-        case 0:
-            // adv - division out to A
-            do_adv( *combo, registers );
-            break;
-        case 1:
-            // bxl - bitwise OR w/ operand
-            do_bxl( *combo, registers );
-            break;
-        case 2:
-            // bst - mod 8
-            do_bst( *combo, registers );
-            break;
-        case 3:
-            // jnz - jump if not zero
-            if( registers[ REG_A ] )
-            {
-                op = std::next( op_codes.cbegin(), ( *combo ) );
-                continue;
-            }
-            break;
-        case 4:
-            // bxc - bitwise OR on B & C
-            do_bxc( *combo, registers );
-            break;
-        case 5:
-            // out - output
-            do_out( *combo, registers, output );
-            break;
-        case 6:
-            // bdv - division out to B
-            do_bdv( *combo, registers );
-            break;
-        case 7:
-            // cdv - division out to C
-            do_cdv( *combo, registers );
-            break;
-        }
-
-        op = std::next( op, 2 );
-    }
-}
-
 int main()
 {
-    std::array<uint32_t, 3> registers;
+    std::array<uint64_t, 3> registers;
     std::vector<uint8_t> op_codes;
     if( !read_input( registers, op_codes ) )
     {
         return -1;
     }
 
-    std::string output;
-    execute( registers, op_codes, output );
+    computer comp( registers );
+    comp.run( op_codes );
 
-    std::cout << output << std::endl;
+    for( auto v : comp.get_output() )
+    {
+        std::cout << std::to_string( v ) << ",";
+    }
+    std::cout << std::endl;
 }
