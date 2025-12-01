4483c15d0640b7389f8246d565efa07d5d62fff1
Steve Aarnio <saarnio@qti.qualcomm.com>
2025-12-01 11:30:33 -0800

    Advent of Code 2025 - d2
    Initial commit for AoC 2025 Day 2.

---
+++ AdventOfCode25/Day02/Day02.cpp
@@ -0,0 +1,26 @@
+// Day02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <fstream>
+#include <iostream>
+
+static bool read_input( void )
+{
+    // Open the input file and read the data.
+    // Step 1: open the input file.
+    // Assumes the input file present in the same folder as the executable.
+    std::ifstream file( ".\\Data\\Input_test.txt" );
+
+    // Step 2: read each line and create the input data.
+
+    // Step 3: return success or failure.
+    return true;
+}
+
+int main()
+{
+    if( !read_input() )
+    {
+        return -1;
+    }
+}
