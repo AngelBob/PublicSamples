c53aacf422e0a4cb9ee3abe496a03614c998f186
Steve Aarnio <saarnio@hotmail.com>
2025-12-01 23:03:04 -0800

    Advent of Code 2025 - d3
    Initial commit for AoC Day 3.

---
+++ AdventOfCode25/Day03/Day03.cpp
@@ -0,0 +1,25 @@
+// Day03.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <fstream>
+#include <iostream>
+
+static bool read_input( void )
+{
+    // Open the input file and read the data.
+    // Step 1: open the input file.
+    // Assumes the input file is present in a sub-folder.
+    std::ifstream file( ".\\Data\\Input_test.txt" );
+
+    // Step 2: read the line and create the input data.
+
+    // Step 3: return success or failure.
+    return true;
+}
+
+int main()
+{
+    if( !read_input() )
+    {
+        return -1;
+    }
+}
