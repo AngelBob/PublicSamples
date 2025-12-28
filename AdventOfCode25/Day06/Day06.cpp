136bb5e6d4fd52f30655c08b47d944fd695c5762
Steve Aarnio <saarnio@hotmail.com>
2025-12-27 22:00:55 -0800

    Advent of Code 2025 - d6p2
    Solution for day 6 part 2.
    Find the answer to the math problem in each column. This time the number
    are oriented vertically rather than horizontally.

--- AdventOfCode25/Day06/Day06.cpp
+++ AdventOfCode25/Day06/Day06.cpp
@@ -111,6 +111,41 @@ static void process_horizontal_rows(
     transposed.emplace_back( std::move( new_row ) );
 }
 
+static void process_vertical_columns(
+    size_t max_col,
+    std::vector<std::string>& input,
+    std::vector<std::list<uint64_t>>& transposed
+)
+{
+    // Numbers are arranged vertically across all rows with
+    // a maximum width of max_col.
+    std::list<uint64_t> new_row;
+    for( size_t col = 0; col < max_col; ++col )
+    {
+        uint64_t new_value = 0;
+        for( size_t row = 0; row < input.size(); ++row )
+        {
+            if( input[ row ][ 0 ] != ' ' )
+            {
+                uint64_t this_digit = std::atoll( input[ row ].substr( 0, 1 ).c_str() );
+                new_value = ( new_value * 10 ) + this_digit;
+            }
+            input[ row ].erase( 0, 1 );
+        }
+
+        new_row.emplace_back( new_value );
+    }
+
+    for( auto& row : input )
+    {
+        // Clear out the now leading space
+        row.erase( 0, 1 );
+    }
+
+    transposed.emplace_back( std::move( new_row ) );
+}
+
+template<bool is_horizontal>
 void transpose(
     std::vector<std::string>& input,
     std::vector<std::list<uint64_t>>& transposed
@@ -122,13 +157,20 @@ void transpose(
         size_t max_col = get_column_width( input );
 
         // Extract the values one column at a time.
-        process_horizontal_rows( max_col, input, transposed );
+        if constexpr( is_horizontal )
+        {
+            process_horizontal_rows( max_col, input, transposed );
+        }
+        else
+        {
+            process_vertical_columns( max_col, input, transposed );
+        }
     }
 }
 
 static uint64_t do_calculation(
-    std::list<char>& ops,
-    std::vector<std::list<uint64_t>>& numbers
+    std::list<char> ops, // <-- need a copy here, this is done twice
+    std::vector<std::list<uint64_t>>& numbers // <-- ref ok, it's two different vectors
 )
 {
     uint64_t sum = 0;
@@ -171,12 +213,18 @@ int main()
     {
         return -1;
     }
+    std::vector<std::string>input1b( input1 );
 
     // Process the input data.
     std::vector<std::list<uint64_t>> numbers;
-    transpose( input1, numbers );
+    transpose<true>( input1, numbers );
 
     // Do the calculations.
     uint64_t sum = do_calculation( input2, numbers );
+    std::cout << "The sum is: " << sum << std::endl;
+
+    // Process the input data a second time.
+    transpose<false>( input1b, numbers );
+    sum = do_calculation( input2, numbers );
     std::cout << "The sum is: " << sum << std::endl;
 }
