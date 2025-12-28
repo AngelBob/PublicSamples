bfc1b39e419e8b291b979e2d5e55bf0dcab7e381
Steve Aarnio <saarnio@hotmail.com>
2025-12-27 22:17:05 -0800

    Advent of Code 2025 - d7
    Initial commit for AoC Day 7.

---
+++ AdventOfCode25/Day07/Day07.cpp
@@ -0,0 +1,31 @@
+// Day07.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <fstream>
+#include <iostream>
+#include <string>
+
+static bool read_input( void )
+{
+    // Open the input file and read the data.
+    // Step 1: open the input file.
+    // Assumes the input file is present in a sub-folder.
+    std::ifstream file( ".\\Data\\Input_test.txt" );
+
+    // Step 2: read the lines and create the input data.
+    while( !file.eof() )
+    {
+        std::string line;
+        std::getline( file, line );
+    }
+
+    // Step 3: return success or failure.
+    return true;
+}
+
+int main()
+{
+    if( !read_input() )
+    {
+        return -1;
+    }
+}
